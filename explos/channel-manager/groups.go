1d3b991f7fbf5199459a6d9378baf5d8dc83a387
ismael FALL <ismael.fall@epitech.eu>
2022-08-10 19:16:07 +0200

    fix: clean up

--- explos/channel-manager/groups.go
+++ explos/channel-manager/groups.go
@@ -5,15 +5,16 @@ import (
 	"berty.tech/berty/v2/go/pkg/messengertypes"
 )
 
-func bertyBotCreateGroup(name string) (error, *messengertypes.BertyLink) {
+func bertyBotCreateGroup(name string) (*messengertypes.BertyLink, error) {
 	g, _, err := bertyprotocol.NewGroupMultiMember()
 	if err != nil {
-		return err, nil
+		return nil, err
 	}
+
 	group := &messengertypes.BertyGroup{
 		Group:       g,
 		DisplayName: name,
 	}
 	link := group.GetBertyLink()
-	return nil, link
+	return link, nil
 }
