c26f740ea24b747368f66f7c43181714332602cf
Mikatech <mikael.vallenet@epitech.eu>
2022-08-25 10:26:49 +0200

    tests: add channel & add user
    Signed-off-by: Mikatech <mikael.vallenet@epitech.eu>

--- explos/channel-manager/sqlite_test.go
+++ explos/channel-manager/sqlite_test.go
@@ -17,3 +17,21 @@ func TestNewSqliteDB(t *testing.T) {
 		t.Error("Can't remove tmp.db")
 	}
 }
+
+func TestAddUser(t *testing.T) {
+	var user User
+	err := testQueries.AddUser("pubKeyTest")
+	require.NoError(t, err)
+	testQueries.db.First(&user)
+	require.Equal(t, "pubKeyTest", user.BertyPubKey)
+}
+
+func TestAddChannel(t *testing.T) {
+	var channel Channel
+	err := testQueries.AddChannel("workspace_test", "channel_test", "link_test")
+	require.NoError(t, err)
+	testQueries.db.Preload("Workspace").First(&channel)
+	require.Equal(t, "workspace_test", channel.Workspace.Name)
+	require.Equal(t, "channel_test", channel.Name)
+	require.Equal(t, "link_test", channel.BertyLink)
+}
