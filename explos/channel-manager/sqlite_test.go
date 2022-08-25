312db9e6e6f8f24e75163022b0182509c62162b0
Mikatech <mikael.vallenet@epitech.eu>
2022-08-25 09:38:47 +0200

    tests: sqlite db creation
    Signed-off-by: Mikatech <mikael.vallenet@epitech.eu>

--- explos/channel-manager/sqlite_test.go
+++ explos/channel-manager/sqlite_test.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
+
+func TestNewSqliteDB(t *testing.T) {
+	db, err := NewSqliteDB("tmp.db")
+	require.NoError(t, err)
+	require.NotNil(t, db)
+	_, err = os.Stat("tmp.db")
+	require.NoError(t, err)
+	err = os.Remove("tmp.db")
+	if err != nil {
+		t.Error("Can't remove tmp.db")
+	}
+}
